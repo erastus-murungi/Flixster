b5186c332b57cbda5fff98328751fef949be4fa7
Erastus Murungi <erastusmurungi@fb.com>
2020-06-26 23:40:39 -0700

    walkthrough.gif

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -6,7 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.DateFormat;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
